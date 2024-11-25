9dde2e6e6e4da5c37df808b911f5b5c4bc054ceb
Nistell <sharofmk@gmail.com>
2024-11-24 22:26:15 -0500

    static files

---
+++ js/auth.js
@@ -0,0 +1,28 @@
+
+const form = document.querySelector('#form_auth');
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const username = document.querySelector('#login').value;
+    const password = document.querySelector('#pass').value;
+
+    const result = await fetch("/", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            username,
+            password
+        })
+    });
+
+    if (result.status === 200) { // Успешная авторизация
+        const data = await result.json();
+        localStorage.setItem("token", data.token); // Сохраняем токен
+        window.location.href = "/admin.html"; // Перенаправление на admin.html
+    } else {
+        console.log("Authorization failed");
+    }
+});
