3fc39f60b6ddffa4ad02cd7081562121e5537f43
logiCCyber <117324079+logiCCyber@users.noreply.github.com>
2024-11-25 14:32:20 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,5 +1,3 @@
-// auth.js
-
 const form = document.querySelector('#form_auth');
 
 form.addEventListener('submit', async (e) => {
@@ -13,51 +11,12 @@ form.addEventListener('submit', async (e) => {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ username, password })
+        body: JSON.stringify({
+            username,
+            password
+        })
     });
-
     const data = await result.json();
     console.log(data);
-
-    if (result.ok) {
-        localStorage.setItem("token", data.token);
-        if (data.role === "admin") {
-            window.location.href = "/admin"; // Переход на страницу администратора
-        } else if (data.role === "client") {
-            window.location.href = "/client"; // Переход на страницу клиента
-        }
-    } else {
-        alert("Ошибка авторизации");
-    }
-
-    document.querySelector('#login').value = "";
-    document.querySelector('#pass').value = "";
+    
 });
-
-// Проверка токена перед доступом к защищенным маршрутам
-const checkAuth = async () => {
-    const token = localStorage.getItem("token");
-
-    if (token) {
-        const response = await fetch("/admin", {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        });
-
-        if (response.ok) {
-            console.log('Token is valid');
-        } else {
-            console.log('Token is invalid');
-            window.location.href = "/"; // Перенаправление, если токен недействителен
-        }
-    } else {
-        window.location.href = "/"; // Перенаправление на страницу входа, если нет токена
-    }
-};
-
-// Вызываем проверку на защищенных страницах
-if (window.location.pathname === '/admin' || window.location.pathname === '/client') {
-    checkAuth();
-}
