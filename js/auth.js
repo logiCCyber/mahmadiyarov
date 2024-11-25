29a17094c8959ab4242a4e4806fb0da26fc4ad3b
logiCCyber <117324079+logiCCyber@users.noreply.github.com>
2024-11-25 14:19:04 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,34 +1,26 @@
-import jwt_decode from 'jwt-decode';
-
-form.addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    const username = document.querySelector('#login').value;
-    const password = document.querySelector('#pass').value;
-
-    const result = await fetch("/", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            username,
-            password
-        })
-    });
-
-    const data = await result.json();
-    
-    if (result.ok) {
-        // Сохраняем токен в localStorage
-        localStorage.setItem("token", data.token);
-        console.log(data);
-        if(data.role === "admin") {
-            window.location.href = "/admin";
-        } else if(data.role === "client") {
-            window.location.href = "/client";
-        }
-    } else {
-        console.log("Authorization failed");
-    }
+const result = await fetch("/", {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        username,
+        password
+    })
 });
+
+const data = await result.json();
+console.log(data);  // Логируем данные, чтобы удостовериться, что они правильные
+
+if (result.ok) {
+    // Сохраняем токен в localStorage
+    localStorage.setItem("token", data.token);
+
+    if (data.role === "admin") {
+        window.location.href = "/admin";
+    } else if (data.role === "client") {
+        window.location.href = "/client";
+    }
+} else {
+    console.log("Authorization failed");
+}
