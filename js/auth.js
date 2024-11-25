529265d37cd8da0b0ea3a27ff8f55dcaae5d28dc
logiCCyber <117324079+logiCCyber@users.noreply.github.com>
2024-11-25 13:57:51 -0500

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -18,13 +18,6 @@ form.addEventListener('submit', async (e) => {
     });
 
     const data = await result.json();
-    
-    const protectedRoute = await fetch("/admin", {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${token}`,
-            },
-        });
 
     if (result.ok) {
         // Сохраняем токен в localStorage
