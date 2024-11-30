9473c7c364d466e82d24e6e57f4f375b8b0de767
logiCCyber <117324079+logiCCyber@users.noreply.github.com>
2024-11-30 18:20:13 -0500

    Update employee.js

--- js/employee.js
+++ js/employee.js
@@ -1,12 +1,12 @@
 const form = document.querySelector('#form_reg');
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
-    const name = document.querySelector("firstName").value;
-    const user = document.querySelector("username").value;
-    const pass = document.querySelector("password").value;
-    const confirm = document.querySelector("confirm").value;
-    const job = document.querySelector("job").value;
-    const salary = document.querySelector("salary").value;
+    const name = document.querySelector("#firstName").value;
+    const user = document.querySelector("#username").value;
+    const pass = document.querySelector("#password").value;
+    const confirm = document.querySelector("#confirm").value;
+    const job = document.querySelector("#job").value;
+    const salary = document.querySelector("#salary").value;
 
     const response = await fetch("/registration", {
        method: "POST",
