7d6c4108fd1ae9ca2f21a88682c022fd4f6bf529
logiCCyber <117324079+logiCCyber@users.noreply.github.com>
2024-12-01 18:22:54 -0500

    Update employee.js

--- js/employee.js
+++ js/employee.js
@@ -1,29 +1,28 @@
 const form = document.querySelector('#form_reg');
 form.addEventListener('submit', async (e) => {
     e.preventDefault();
-    console.log(document.querySelector("#firstName").value);
-    // const name = document.querySelector("#firstName");
-    // const user = document.querySelector("#username");
-    // const pass = document.querySelector("#password");
-    // const confirm = document.querySelector("#confirm");
-    // const job = document.querySelector("#job");
-    // const salary = document.querySelector("#salary");
+    const name = document.querySelector("#firstName");
+    const user = document.querySelector("#username");
+    const pass = document.querySelector("#password");
+    const confirm = document.querySelector("#confirm");
+    const job = document.querySelector("#job");
+    const salary = document.querySelector("#salary");
 
-    // const response = await fetch("/registration", {
-    //    method: "POST",
-    //    headers: {
-    //         "Content-Type": "application/json"
-    //    },
-    //    body: JSON.stringify({
-    //         name.value,
-    //         user.value,
-    //         pass.value,
-    //         confirm.value,
-    //         job.value,
-    //         salary.value
-    //    }) 
-    // });
+    const response = await fetch("/registration", {
+       method: "POST",
+       headers: {
+            "Content-Type": "application/json"
+       },
+       body: JSON.stringify({
+            name.value,
+            user.value,
+            pass.value,
+            confirm.value,
+            job.value,
+            salary.value
+       }) 
+    });
 
-    // const data = await response.json();
-    // console.log(data);    
+    const data = await response.json();
+    console.log(data);    
 });
