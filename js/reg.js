c96ca2ca9a845f1f222323361143a8c47214fa4e
logiCCyber <117324079+logiCCyber@users.noreply.github.com>
2024-12-28 20:36:30 -0500

    Update reg.js

--- js/reg.js
+++ js/reg.js
@@ -1,22 +1,39 @@
+(() => {
+  'use strict'
 
-// document.querySelector("#form").addEventListener("submit", async (e) => {
-//     e.preventDefault();
+  // Получаем все формы с классом .needs-validation
+  const forms = document.querySelectorAll('.needs-validation')
 
-//     const response = await fetch("/login", {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json",
-//         },
-//         body: JSON.stringify({
-//             firstName: document.getElementById("firstName").value,
-//             lastName: document.getElementById("lastName").value,
-//             password: document.getElementById("password").value,
-//             salary: document.getElementById("salary").value,
-//         })
-//     });
+  // Перебираем формы и добавляем обработчик события
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', async (event) => {
+      // Останавливаем отправку формы, если она невалидна
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      } else {
+        // Если форма валидна, выполняем асинхронный запрос
+        event.preventDefault(); // Останавливаем стандартную отправку формы
 
-//     const data = await response.json();
-//     console.log(data);
-// });
+        const response = await fetch("/login", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            firstName: document.getElementById("firstName").value,
+            lastName: document.getElementById("lastName").value,
+            password: document.getElementById("password").value,
+            salary: document.getElementById("salary").value,
+          })
+        });
 
-console.log("asdfasdf");
+        const data = await response.json();
+        console.log(data);
+      }
+
+      // Добавляем класс для отображения ошибок валидации
+      form.classList.add('was-validated')
+    }, false)
+  })
+})();
